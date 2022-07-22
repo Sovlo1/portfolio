e12952fb125d9b9eb53060f28e6978b5ff3b2886
Sovlo <94002236+Sovlo1@users.noreply.github.com>
2022-07-22 06:40:00 +0200

    updated component, started working on layout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor(private viewPortScroller: ViewportScroller) {}
 
   ngOnInit(): void {
+    this.viewPortScroller.setOffset([0, 50]);
   }
 
+  goToMain(element: string) {
+    this.viewPortScroller.scrollToAnchor(element);
+  }
+
+  goToProjects(element: string) {
+    this.viewPortScroller.scrollToAnchor(element);
+  }
+
+  goToAbout(element: string) {
+    this.viewPortScroller.scrollToAnchor(element);
+  }
+
+  goToContact(element: string) {
+    this.viewPortScroller.scrollToAnchor(element);
+  }
 }
