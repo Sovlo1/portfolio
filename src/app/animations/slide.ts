41fadc747df8b4a670a717c95471096a4ea6f6b9
Sovlo <94002236+Sovlo1@users.noreply.github.com>
2022-09-22 02:15:45 +0200

    small update

--- src/app/animations/slide.ts
+++ src/app/animations/slide.ts
@@ -11,23 +11,41 @@ import {
 export const slideInAnimation = trigger('routeAnimations', [
   transition('* <=> *', [
     style({ position: 'relative' }),
-    query(':enter, :leave', [
-      style({
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-      }),
-    ]),
-    query(':enter', [style({ left: '-100%' })]),
-    query(':leave', animateChild()),
+    query(
+      ':enter, :leave',
+      [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+        }),
+      ],
+      { optional: true }
+    ),
+    query(':enter', [style({ left: '-100%' })], { optional: true }),
+    query(':leave', animateChild(), { optional: true }),
     group([
-      query(':leave', [
-        animate('700ms ease-in-out', style({ transform: 'translateX(100%)' })),
-      ]),
-      query(':enter', [
-        animate('700ms ease-in-out', style({ transform: 'translateX(100%)' })),
-      ]),
+      query(
+        ':leave',
+        [
+          animate(
+            '700ms ease-in-out',
+            style({ transform: 'translateX(100%)' })
+          ),
+        ],
+        { optional: true }
+      ),
+      query(
+        ':enter',
+        [
+          animate(
+            '700ms ease-in-out',
+            style({ transform: 'translateX(100%)' })
+          ),
+        ],
+        { optional: true }
+      ),
     ]),
   ]),
 ]);
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,4 @@
-import { ViewportScroller } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DarkModeService } from '../services/dark-mode.service';
 
@@ -13,34 +12,25 @@ export class HeaderComponent implements OnInit {
   public dirtyMenu: boolean = false;
   public dark!: boolean;
   public animation: boolean = false;
+  public currentWindowWidth!: number;
+  public displayButtons: boolean = false;
+
+  @HostListener('window:resize')
+  onResize() {
+    this.currentWindowWidth = window.innerWidth;
+  }
+
   constructor(private darkMode: DarkModeService, private router: Router) {}
 
   ngOnInit(): void {
-    this.dark = this.darkMode.checkDarkMode();
-    if (this.dark === true) {
-      document.body.classList.toggle('light-mode');
-    }
-  }
-
-  DarkOrLightMode(): void {
-    this.dark = this.darkMode.changeMode();
-    document.body.classList.toggle('light-mode');
+    this.currentWindowWidth = window.innerWidth;
   }
 
   goTo(element: string): void {
     this.router.navigate(['./' + element]);
   }
 
-  openMenu(): void {
-    this.isMenuOpen = !this.isMenuOpen;
-    this.animation = !this.animation;
-    this.dirtyMenu = true;
-  }
-
-  closeMenu(): void {
-    if (this.isMenuOpen === true) {
-      this.animation = !this.animation;
-      this.isMenuOpen = false;
-    }
+  showButtons() {
+    this.displayButtons = !this.displayButtons;
   }
 }
